0288f4ee6a11d6c9bc2d39ece35a2b72f2d40251
gbelther <belthergabriel@gmail.com>
2022-11-13 12:50:00 -0300

    create base project

---
+++ packages/docs/src/stories/Test.stories.tsx
@@ -0,0 +1,9 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Test } from '@onyma-ds/react'
+
+export default {
+  title: 'Test',
+  component: Test,
+} as Meta;
+
+export const Primary: StoryObj = {};
---
+++ packages/react/src/index.tsx
@@ -0,0 +1,5 @@
+import * as SC from "./styles";
+
+export const Test = () => {
+  return <SC.Container>Olá mundo</SC.Container>;
+};
